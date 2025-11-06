3466eae5b63fa7ee7d29067f8dd5e1d44856d234
Divyam Gupta <153823625+Divyamgupta785@users.noreply.github.com>
2025-11-06 21:15:52 +0530

    Time: 2 ms (84.11%) | Memory: 47.6 MB (5.75%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-        int n = nums.length;
+        int maxreach = 0;
+        int last = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            // If we can't reach this index
-            if (i > maxReach) return false;
+        for(int i=0;i<last;i++){
+            if(i>maxreach){
+                return false;
+            }
+            maxreach = Math.max(maxreach,i+nums[i]);
 
-            // Update the farthest we can reach
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            // If we can reach or go beyond the last index
-            if (maxReach >= n - 1) return true;
+            if(maxreach>=last-1){
+                return true;
+            }
         }
-
-        return true; // if loop finishes, last index is reachable
+        return false;
     }
-    }
+}
